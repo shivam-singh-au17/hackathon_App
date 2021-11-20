b220af02819bffde1577d64365dc7268df742d01
Shivam Singh <shivamsingh4458@gmail.com>
2021-11-20 10:14:32 +0530

    adding home background

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import Accordion from './components/Accordion/Accordion';
+import HelpPop from './components/HelpPop/HelpPop';
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Navbar />
+      <Home />
+      <Accordion />
+      <HelpPop />
     </div>
   );
 }
