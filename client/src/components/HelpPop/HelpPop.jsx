3b26c54cfff3baf5823443c42b0217ccca683fdc
Shivam Singh <shivamsingh4458@gmail.com>
2021-11-20 10:13:16 +0530

    adding help js file

---
+++ client/src/components/HelpPop/HelpPop.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import "./HelpPop.css";
+
+const HelpPop = () => {
+  const submitHelpPopup = (e) => {
+    e.preventDefault();
+    const form = document.getElementById("contactUsForm-helpPopup");
+    let name_popup = form.name_popup.value;
+    let email_popup = form.email_popup.value;
+    let type_popup = form.type_popup.value;
+    let tell_us_more_popup = form.tell_us_more_popup.value;
+    if (name_popup === "") {
+      alert("Please Type your name");
+    } else if (email_popup === "") {
+      alert("Please Type your email");
+    } else if (type_popup === "") {
+      alert("Please select a type");
+    } else if (tell_us_more_popup === "") {
+      alert("Please tell us more");
+    } else {
+      form.name_popup.value = "";
+      form.email_popup.value = "";
+      form.type_popup.value = "";
+      form.tell_us_more_popup.value = "";
+      document.getElementsByClassName("thank_you")[0].classList.toggle("hide");
+    }
+  };
+
+  const showContactPopup = () => {
+    let contactPopup = document.getElementsByClassName(
+      "contactUsForm-helpPopup-cont"
+    )[0];
+    contactPopup.classList.toggle("active");
+  };
+
+  return (
+    <div>
+      <div className="contactUsForm-helpPopup-cont">
+        <div className="contactUsForm-helpPopup-heading-cont">
+          <h3 className="contactUsForm-helpPopup-heading">
+            Contact CheapDelivery Support
+          </h3>
+        </div>
+        <form id="contactUsForm-helpPopup">
+          <label htmlFor="name-popup">Your name</label>
+          <input type="text" name="name" id="name_popup" />
+
+          <label htmlFor="email-popup">
+            Email <span>*</span>
+          </label>
+          <input type="text" name="email" id="email_popup" />
+
+          <label htmlFor="tell-us-more">
+            Tell us more! <span>*</span>
+          </label>
+          <textarea
+            name="tell-us-more"
+            id="tell_us_more_popup"
+            cols="30"
+            rows="10"
+          ></textarea>
+          <button onClick={submitHelpPopup}>Send</button>
+          <p className="hide thank_you" style={{ color: "green" }}>
+            Thank you for contacting us. We will get back to you!
+          </p>
+        </form>
+      </div>
+      <div className="helpPopup" onClick={showContactPopup}>
+        <i className="far fa-question-circle"></i> Help
+      </div>
+    </div>
+  );
+};
+
+export default HelpPop;
