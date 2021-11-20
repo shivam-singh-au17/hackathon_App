758780bd1268e5a0cf36b92db9e708fb90ff0811
asutoshb <beheraasutosh132@gmail.com>
2021-11-21 01:31:59 +0530

    added new partner

--- client/src/components/Partner/Partner.jsx
+++ client/src/components/Partner/Partner.jsx
@@ -8,14 +8,26 @@ export default function BasicPopover() {
       const initValue = {
          "company_name":"",
         "company_logo":"",
-        "pincodes_serving":[],
-        "pricing":{}
-        
+        "pincodes_serving":["422001", "123333"],
+        "pricing":{"gte500": "",
+                "gte1000" : "",
+                "lt500" : "",
+        },
+         "trucks" : [ 
+                    "61989af06d0a0337acdfec8a",
+                    "6198b1fca42d78177cd4230a",
+                    "6198b2d7a42d78177cd4230b",
+                    "6198b2dfa42d78177cd4230c",
+                    "6198b45ea42d78177cd4230d",
+                    "6198b468a42d78177cd4230e"
+                ]
+
     }
 
     const [formData, setFormData] = React.useState(initValue)
 
         const handleChange = (e) => {
+            e.preventDefault();
 
         const {name, value, type, checked} = e.target;
         //console.log(e.target.checked)
@@ -30,7 +42,7 @@ export default function BasicPopover() {
         else if(name === 'pricing')
         {
             setFormData({
-            ...formData, [name]:  {"gte1000":value},
+            ...formData, [name]:  {"gte1000" : +value, "gte500": value-1, "lt500" : value-2 },
             }) 
         }
         else{
@@ -83,20 +95,23 @@ export default function BasicPopover() {
                 <label>Price</label>
                 <input onChange={handleChange} type="number" name="pricing" placeholder="Enter Price"/>
                 <button 
-                onClick={()=>{
+                onClick={(e)=>{
+                    e.preventDefault();
                     const data = {
                         company_name: formData.company_name,
                         company_logo: formData.company_logo,
                         pincodes_serving: formData.pincodes_serving,
                         pricing: formData.pricing,
                     }
-                    fetch("https://mysterious-depths-33415.herokuapp.com/company/price",{
+                    fetch("http://localhost:5000/company",{
                         method: "POST",
                         body: JSON.stringify(data),
                         headers: {
                             "Content-Type": "application/json",
                         }
-                    })                                
+                    })
+                    .then((res)=>alert("working"))
+                    .catch((err)=>alert(err.message))
                     console.log("formData", formData)
                 }}
                 >Submit</button>
