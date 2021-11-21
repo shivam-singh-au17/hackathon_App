a338abb926f86d03d4fc49fd7ad00f62042d5067
Shivam Singh <shivamsingh4458@gmail.com>
2021-11-21 11:43:24 +0530

    final changes

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -41,11 +41,6 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
                 <span
                   className="nav-link active"
                   type="button"
